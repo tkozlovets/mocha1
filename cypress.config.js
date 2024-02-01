cec171dd3be0f8ecadd18096a40233f484e820f9
“tkozlovets” <tkozlovets@ukr.net>
2024-02-01 16:38:05 +0100

    homework added

--- cypress.config.js
+++ cypress.config.js
@@ -1,11 +1,10 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  viewportWidth: 1920,
+  viewportHeight: 1080,
+  watchForFileChanges: false,
   e2e: {
-    retries: {
-      runMode: 2,
-      openMode: 2
-    },
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
