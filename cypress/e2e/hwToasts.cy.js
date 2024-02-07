9b6ea98be4c8822208df9f16cd8bce5ae9a42611
“tkozlovets” <tkozlovets@ukr.net>
2024-02-07 12:35:53 +0100

    toast check added

---
+++ cypress/e2e/hwToasts.cy.js
@@ -0,0 +1,56 @@
+let testData= {
+    position1: 'nb-option:contains("top-end")',
+    titleValue: "Tanya toast",
+    contentValue: "Tanya content",
+    timeoutValue: 10000,
+    type1: 'nb-option:contains("success")',
+    rgbValue: 'rgb(96, 175, 32)'
+
+
+}
+
+// let expectedResult= {
+//     icon: 'email',
+//         title: 'test title',
+//         content: 'test content',
+//         color: 'rgb(96, 175, 32)',
+//     // position:
+//
+// }
+
+before(() => {
+    cy.visit('https://sanitarskyi-ngx-admin.herokuapp.com/');
+
+    cy.get('nb-card-header:contains("Material Light")').click();
+
+    cy.get('span:contains("Modal & Overlays")').click();
+
+    cy.get('span:contains("Toastr")').click();
+
+})
+
+it(`Filling out`, () => {
+
+    cy.get('button:contains("top-right")').click();
+    cy.get(testData.position1).click();
+    cy.get('input[name="title"]').clear();
+    cy.get('input[name="title"]').type(`${testData.titleValue}`);
+    cy.get('input[name="content"]').clear();
+    cy.get('input[name="content"]').type(`${testData.contentValue}`);
+    cy.get('input[name="timeout"]').clear();
+    cy.get('input[name="timeout"]').type(`${testData.timeoutValue}`);
+    cy.get('button:contains("primary")').click();
+    cy.get(`${testData.type1}`).click();
+    cy.get('span:contains("Hide on click")').click();
+    cy.get('button:contains("Show toast")').click();
+    cy.get('nb-toast').click();
+
+    cy.get ('nb-toast').should('be.visible');
+    cy.get ('nb-toast').should('contain', `${testData.titleValue}`);
+    cy.get ('nb-toast').should('contain', `${testData.contentValue}`);
+
+    cy.get('nb-toast').should('have.css', 'background-color').and('eq',
+        `${testData.rgbValue}`);
+
+
+})
